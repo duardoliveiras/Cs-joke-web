8414c9a57f090df022c4cb1f339e9e437dab66b5
duardoliveiras <duardooliveiras@gmail.com>
2024-02-02 12:38:12 +0000

    adjust atributtes

--- WebJokes/Models/Joke.cs
+++ WebJokes/Models/Joke.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Question { get; set; }
         public string Answer { get; set; }
-        public string Author { get; set; }
 
 
         public Joke()
